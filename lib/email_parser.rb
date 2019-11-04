7f4b084635d8c0f011e72c2bf407e76b181080d9
Deanna Kelley <deanna.georges@gmail.com>
2019-11-04 00:44:39 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,7 +10,7 @@ class EmailAddressParser
   end
   
   def parse
-    all_emails = @emails.split.collect do |address|
+    all_emails = emails.split.collect do |address|
       address.split(", ")
     end
     final_array = all_emails.flatten.uniq
