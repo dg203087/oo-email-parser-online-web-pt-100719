2a363039b97d351af42ff419133d62223f1e0402
Deanna Kelley <deanna.georges@gmail.com>
2019-11-04 00:21:02 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -5,10 +5,10 @@
 def EmailAddressParser
   attr_accessor 
   
-  def self.parse(email)
-    song = self.new
+  def self.parse(emails)
+    
     song.name = (file_name.split(" - ")[1].chomp(".mp3")) 
-      #splits input and sets the second instance in the array (name of song) equal to song.name. Removes the file type
+      
     song.artist_name = (file_name.split(" - ")[0]) 
       #splits input and sets the first instance in the array (artist name) equal to artist_name 
     song 
